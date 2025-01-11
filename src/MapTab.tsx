b040b4c63fab8cabf7ee6c807e7f17da64c4d8b5
superrache <superrache@hotmail.fr>
2025-01-11 22:26:03 +0100

    feat: add map

--- src/MapTab.tsx
+++ src/MapTab.tsx
@@ -1,9 +1,75 @@
-import './MapTab.css'
+import { useEffect, useRef } from 'react'
 import logo from '/osmtree.svg' // also favicon
+import { Map, StyleSpecification, NavigationControl, ScaleControl, GeolocateControl } from 'maplibre-gl'
+import 'maplibre-gl/dist/maplibre-gl.css'
+import './MapTab.css'
 
 const MapTab = ({switchToAttributes}) => {
+    const mapContainer = useRef<HTMLDivElement | null>(null)
+    const map = useRef<Map | null>(null)
+    
+    const lng = 5.04147
+    const lat = 47.32158
+    const zoom = 14
+
+    const orthoStyle: StyleSpecification = {
+        version: 8,
+        name: "bdortho",
+        sources: {
+            bdortho: {
+                type: 'raster',
+                tiles: ['https://data.geopf.fr/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&STYLE=normal&FORMAT=image/jpeg&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}'],
+                tileSize: 256,
+                attribution: "<a href='https://geoservices.ign.fr/bdortho' title='BD ORTHO®' target='_blank' class='jawg-attrib'>&copy; <b>IGN</b> BD ORTHO®</a>"
+            }
+        },
+        layers: [
+            {
+                id: 'tiles',
+                type: 'raster',
+                source: 'bdortho',
+                minzoom: 0,
+                maxzoom: 22
+            }
+        ]
+    }
+
+    useEffect(() => {
+        if (map.current) return
+
+        if (mapContainer.current) {
+            map.current = new Map({
+                container: mapContainer.current,
+                style: orthoStyle,
+                center: [lng, lat],
+                zoom: zoom
+            })
+
+            map.current.addControl(
+                new NavigationControl({
+                    visualizePitch: true,
+                    showZoom: true,
+                    showCompass: true
+                })
+            )
+
+            const scale = new ScaleControl({
+                maxWidth: 80,
+                unit: 'metric'
+            })
+            map.current.addControl(scale)
+
+            map.current.addControl(new GeolocateControl({
+                positionOptions: {
+                    enableHighAccuracy: true
+                },
+                trackUserLocation: true
+            }))
+        }
+    }, [lng, lat, zoom])
+
     return (
-        <div className="map">
+        <div className="map" ref={mapContainer}>
             <a className="title">
                 <img src={logo} className="logo" alt="osmtree" />
                 osmtree
